dfc4b2138aa20a42b1eb33883e36b787f227b6c4
MamreyAisultan <mamreyaisultan123@gmail.com>
2025-09-06 12:40:36 +0500

    feat: final commit

--- main.py
+++ main.py
@@ -9,7 +9,26 @@ secret = random.randint(1, 100)
 attempts_left = 7
 
 def guess():
-    pass
+    global attempts_left
+    guess = entry.get()
+
+    if not guess.isdigit():
+        result.config(text="Please enter a valid number.")
+        return
+    
+    guess = int(guess)
+    if guess == secret:
+        result.config(text="Угадал!")
+        button.config(state="disabled")
+    else:
+        attempts_left -= 1
+        if attempts_left == 0:
+            result.config(text=f"Игра окончена! Загаданное число: {secret}")
+            button.config(state="disabled")
+        elif guess < secret:
+            result.config(text=f"Загаданное число больше. Попыток осталось: {attempts_left}")
+        else:
+            result.config(text=f"Загаданное число меньше. Попыток осталось: {attempts_left}")
 
 entry = tk.Entry(window)
 entry.pack(pady=10)
